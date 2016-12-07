5a56627427f3589cdf468a704a18d36cedbdf580
Alistair Duff <alistair.g.duff@gmail.com>
2016-12-07 16:32:32 +0000

    basically just made the whole thing...

--- app/controllers/tea_groups_controller.rb
+++ app/controllers/tea_groups_controller.rb
@@ -1,7 +1,12 @@
 class TeaGroupsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
     @tea_groups = TeaGroup.all
   end
+
+  def show
+    @tea_group = TeaGroup.find(params[:id])
+  end
+
 end
