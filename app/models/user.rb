ed87ea2846a17c7255f4ed6d7921a8c1e688e43b
Luke Breen <l.breen1@outlook.com>
2016-12-07 15:04:58 +0000

    added new user fields to devise form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
   validates :milk_preference, inclusion: { in: ["splash", "dollup", "drown it", "none", "dash"]}
 end
